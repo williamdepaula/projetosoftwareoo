5c50fdd7f0602a4775a5c83940a642f29cef47a7
William de Paula <williampereiradepaula@gmail.com>
2020-07-31 11:16:42 -0400

    Diagramas criados. Projeto Api configurado

---
+++ projetosoftwareoo-api/src/main/java/io/github/williamdepaula/projetosoftwareoo/ProjetosoftwareooApplication.java
@@ -0,0 +1,13 @@
+package io.github.williamdepaula.projetosoftwareoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosoftwareooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetosoftwareooApplication.class, args);
+    }
+
+}
---
+++ projetosoftwareoo-api/src/test/java/io/github/williamdepaula/projetosoftwareoo/ProjetosoftwareooApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.williamdepaula.projetosoftwareoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosoftwareooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
