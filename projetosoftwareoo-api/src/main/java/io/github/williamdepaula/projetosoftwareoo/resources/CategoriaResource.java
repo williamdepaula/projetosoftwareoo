299fd25e8440b9949a6b87a4fdf798660b1e644f
William de Paula <williampereiradepaula@gmail.com>
2020-08-04 09:46:20 -0400

    Adição de Categoria. E primero rest de teste

---
+++ projetosoftwareoo-api/src/main/java/io/github/williamdepaula/projetosoftwareoo/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package io.github.williamdepaula.projetosoftwareoo.resources;
+
+import io.github.williamdepaula.projetosoftwareoo.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+    }
+}
